d35e1e8d00741da7444832bf778caacd6055abf3
adent11 <55511021+adent11@users.noreply.github.com>
2020-01-23 14:26:45 -0500

    Add both control code

--- WiringCode/PIDBox/PIDBox.ino
+++ WiringCode/PIDBox/PIDBox.ino
@@ -10,22 +10,29 @@ LiquidCrystal_I2C lcd(0x3F, 16, 2); // set the LCD address to 0x27 for a 16 char
 // If 0x27 doesn't work, try 0x3F.
 
 // rpm varibles
-
-volatile int interruptCount;
-const int rpsCalcDelay = 200;
-const int oldmillis = 0;
+volatile int interruptCount = 0;
+const int rpmCalcDelay = 200;  //Delay between calculations of rpm
+long oldmillis = 0;
 
 // motor control variables
 const int interruptPin = 3;
 const int motorPin = 6;
-int actualspeed;
-int setPoint;
+int actualSpeed = 0;
+int setPoint = 0;
+int motorPower = 0;
+int error;
+int adjustment; //proportional control adjustment
+
+double kp = 8; //PID constant
+double ki = .005; //PID constant
+double kd = 7; //PID constant
+int oldError, Actual;
+float Derivative, Drive, Integral;
 
 // user control pins
-const int pidSwitch = 5;
-const int potPin = 1; //Analog pin, add later
+const int modeSwitch = 5;
+const int potPin = 0; //Analog
 int initialized = 0;
-
 
 //LED indicator pins
 const int pidLed = 7;
@@ -38,6 +45,8 @@ const int led4 = 13;
 bool ledblink = false;
 int ledStartupDelay = 70;
 
+
+
 void setup()
 {
   Serial.begin(9600);
@@ -46,7 +55,7 @@ void setup()
   lcd.begin(16, 2);
   lcd.clear();
   pinMode(motorPin, OUTPUT);
-  pinMode(pidSwitch, INPUT);
+  pinMode(modeSwitch, INPUT);
   pinMode(pidLed, OUTPUT);
   pinMode(pwrLed, OUTPUT);
   digitalWrite(pwrLed, HIGH);
@@ -54,27 +63,61 @@ void setup()
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
+  pinMode(ledBlink, OUTPUT);
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), count, CHANGE);
+  initialized = digitalRead(modeSwitch);
   initSequence();
+  
 }
+
+
+
 
 void loop()
 {
+  
+  setPoint = map(analogRead(potPin), 0, 1023, 0, 100);
+  lcd.setCursor(0, 0);
+  lcd.print("Setpoint: ");
+  lcd.print(setPoint);
+  lcd.print("       ");
+  lcd.setCursor(0, 1);
+  lcd.print("Speed: ");
+  lcd.print(actualSpeed);
+  lcd.print("         ");
+  
+  if(motorPower > 255){ //ensures that the power isn't above 255
+    motorPower = 255;
+  }
+  if(motorPower < 0){ //ensures that the power isn't below 0
+    motorPower = 0;
+  }
+
+  analogWrite(motorPin, motorPower);
+  Serial.println(motorPower);
+     
+  if (millis() >= oldmillis + rpmCalcDelay) {
+    detachInterrupt(digitalPinToInterrupt(interruptPin));
+    //Serial.println(interruptCount);
+    actualSpeed = map(calcRPM(), 0, 10000, 0 ,100);
+    attachInterrupt(digitalPinToInterrupt(interruptPin), count, CHANGE);
+    interruptCount = 0;
+    oldmillis = millis();
+  }
 
   //No PID loop
-  if (digitalRead(pidSwitch) == LOW) {
+  if (digitalRead(modeSwitch) == LOW) {
     if (initialized == 0) {
       digitalWrite(pidLed, LOW);
       lcd.setCursor(0, 0);
       lcd.print("PID OFF                       ");
       initialized = 1;
-      delay(100);
+      delay(500);
     }
-    if (millis() == oldmillis + rpsCalcDelay) {
-      // motor correction
-    }
-
+    error = setPoint - actualSpeed;
+    adjustment = map(error, -100, 100, -10, 10);
+    motorPower = motorPower + adjustment;
   }
 
 
@@ -85,27 +128,63 @@ void loop()
       lcd.setCursor(0, 0);
       lcd.print("PID ON                     ");
       initialized = 0;
-      delay(100);
+      delay(500);
     }
-    if (millis() == oldmillis + rpsCalcDelay) {
-      // pid code
-    }
+    
+   error = setPoint-actualSpeed;
+   Integral = Integral + (error*rpmCalcDelay);
+   Derivative = (error - oldError)/rpmCalcDelay;
+   motorPower = (error*kp) + (Integral*ki) + (Derivative*kd);
+   oldError = error;
+   delay(rpmCalcDelay);
+   
   }
-  /*if(millis() >= oldmillis+rpsCalcDelay){
-    detachInterrupt(digitalPinToInterrupt(interruptPin), count, CHANGE);
-    //PID CODE GOES HERE
-    //calcRPS()
-    }*/
-
+  
 }
 
 void count()
 {
   interruptCount = interruptCount + 1;
-  //ledblink = !ledblink;
+  ledblink = !ledblink;
+  digitalWrite(ledBlink, ledblink);
 }
+
+
+int calcRPM()
+  {
+  int rpm;
+	rpm = interruptCount*(60000/rpmCalcDelay);
+  return rpm;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void initSequence() {
+  lcd.print("Alden  Dent");
+  delay(500);
+  lcd.setCursor(0, 1);
+  lcd.print("PID Box");
   digitalWrite(ledBlink, HIGH);
   digitalWrite(led1, HIGH);
   delay(ledStartupDelay);
@@ -143,17 +222,25 @@ void initSequence() {
   digitalWrite(led2, HIGH);
   digitalWrite(led3, HIGH);
   digitalWrite(led4, HIGH);
-  delay(500);
+  delay(300);
+  digitalWrite(ledBlink, LOW);
   digitalWrite(led1, LOW);
   digitalWrite(led2, LOW);
   digitalWrite(led3, LOW);
   digitalWrite(led4, LOW);
-  delay(500);
+  delay(300);
+  digitalWrite(ledBlink, HIGH);
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  delay(300);
+  digitalWrite(ledBlink, LOW);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  delay(1000);
+  lcd.clear();
+  delay(1000);
 }
-
-/*int calcRPS(interruptCount)
-  {
-	rps = interruptCount*(1/rpsCalcDelay);
-	attachInterrupt(digitalPinToInterrupt(interruptPin), count, CHANGE);
-  return rps;
-  }*/
